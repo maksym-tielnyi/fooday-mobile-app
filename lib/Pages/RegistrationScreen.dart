48e29fdc59fd16d29e4dd71bd82a941914ebc44c
Vladislav-Martynov <vladyslav.martynov1@nure.ua>
2021-05-10 17:07:44 +0300

    Registration Screen

--- lib/Pages/RegistrationScreen.dart
+++ lib/Pages/RegistrationScreen.dart
@@ -5,17 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:fooday_mobile_app/facebook__icon_icons.dart';
 import '../Models/EntryPage.dart';
 import 'package:fooday_mobile_app/google__icon_icons.dart';
+import 'LoginScreen.dart';
 import 'HomeScreen.dart';
-import 'RegistrationScreen.dart';
 
-class LoginScreen extends StatelessWidget {
+class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _LoginScreenState();
+    return _RegistrationScreenState();
   }
 }
 
-class _LoginScreenState extends StatelessWidget {
+class _RegistrationScreenState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final headerEllipseRadius = Radius.elliptical(80, 40);
@@ -34,12 +34,29 @@ class _LoginScreenState extends StatelessWidget {
                     child: Padding(
                         padding: EdgeInsets.symmetric(vertical: 20),
                         child: Center(
-                            child: Text("Log in",
+                            child: Text("Регистрация",
                                 style:
-                                    Theme.of(context).textTheme.headline6))))),
+                                Theme.of(context).textTheme.headline6))))),
             Container(
               height: 50,
-              margin: EdgeInsetsDirectional.fromSTEB(40, 130, 40, 0),
+              margin: EdgeInsetsDirectional.fromSTEB(40, 110, 40, 0),
+              padding: EdgeInsetsDirectional.fromSTEB(20, 5, 10, 5),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Введите почту",
+                  filled: false,
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+                color: Colors.redAccent,
+              ),
+              // TextBox.fromLTRBD(30, 10, 100, 10, TextDirection.ltr),
+            ),
+            Container(
+              height: 50,
+              margin: EdgeInsetsDirectional.fromSTEB(40, 40, 40, 0),
               padding: EdgeInsetsDirectional.fromSTEB(20, 5, 10, 5),
               child: TextField(
                 decoration: InputDecoration(
@@ -71,35 +88,24 @@ class _LoginScreenState extends StatelessWidget {
               ),
               // TextBox.fromLTRBD(30, 10, 100, 10, TextDirection.ltr),
             ),
-            Row(
-              children: [
-                Container(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context){return RegistrationScreen();}), (route) => false);
-                    },
-                    child: Text(
-                      "Регистрация",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  margin: EdgeInsetsDirectional.fromSTEB(50, 0, 20, 0),
+            Container(
+              height: 50,
+              margin: EdgeInsetsDirectional.fromSTEB(40, 40, 40, 0),
+              padding: EdgeInsetsDirectional.fromSTEB(20, 5, 10, 5),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Повторите пароль",
+                  filled: false,
                 ),
-                Container(
-                  child: TextButton(
-                    onPressed: () {
-                      print("забыли пароль");
-                    },
-                    child: Text(
-                      "Забыли пароль?",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  margin: EdgeInsetsDirectional.fromSTEB(0, 0, 40, 0),
-                ),
-              ],
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+                color: Colors.redAccent,
+              ),
+              // TextBox.fromLTRBD(30, 10, 100, 10, TextDirection.ltr),
             ),
-            SizedBox(height: 100),
+            SizedBox(height: 50),
             Container(
               child: ElevatedButton(
                 onPressed: () {
@@ -111,37 +117,23 @@ class _LoginScreenState extends StatelessWidget {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30))),
                 child: Text(
-                  "Вход",
+                  "Зарегистрироваться",
                 ),
               ),
             ),
-            SizedBox(height: 90),
             Container(
-              height: 50,
-              child: Text(
-                "Войти с помощью:",
-                style: Theme.of(context).textTheme.headline6,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context){return LoginScreen();}), (route) => false);
+                },
+                child: Text(
+                  "Уже есть аккаунт?",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              margin: EdgeInsetsDirectional.fromSTEB(40, 20, 40, 0),
+              margin: EdgeInsetsDirectional.fromSTEB(50, 0, 45, 0),
             ),
-            Row(
-              children: [
-                Container(
-                    margin: EdgeInsetsDirectional.fromSTEB(110, 0, 40, 0),
-                    child: Icon(
-                      Google_Icon.gplus,
-                      size: 50,
-                      color: Colors.redAccent,
-                    )),
-                Container(
-                    margin: EdgeInsetsDirectional.fromSTEB(10, 0, 40, 0),
-                    child: Icon(
-                      Facebook_Icon.facebook,
-                      size: 50,
-                      color: Colors.redAccent,
-                    ))
-              ],
-            ),
+            SizedBox(height: 90),
           ],
         ),
       ),
